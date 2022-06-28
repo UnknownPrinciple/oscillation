264c36e307b1681c06cd893a958b351d9c21beb0
Alexey Raspopov <alexeyraspopov@hotmail.com>
2022-06-28 18:16:22 -0400

    generate package.json

--- packages/oscillation/rollup.config.js
+++ packages/oscillation/rollup.config.js
@@ -8,6 +8,38 @@ export default {
   plugins: [
     multi(),
     size(),
-    copy({ targets: [{ src: ["package.json", "../../LICENSE", "README.md"], dest: "build" }] }),
+    copy({
+      targets: [
+        { src: ["../../LICENSE", "README.md"], dest: "build" },
+        { src: ["package.json"], dest: "build", transform: generatePkg },
+      ],
+    }),
   ],
 };
+
+function generatePkg(contents) {
+  let pkg = JSON.parse(contents.toString());
+  return JSON.stringify(
+    {
+      name: pkg.name,
+      version: pkg.version,
+      description: pkg.description,
+      license: pkg.license,
+      author: pkg.author,
+      homepage: pkg.homepage,
+      repository: pkg.repository,
+      main: pkg.main,
+      module: pkg.module,
+      exports: pkg.exports,
+      type: pkg.type,
+      types: pkg.types,
+      sideEffects: pkg.sideEffects,
+      files: pkg.files,
+      keywords: pkg.keywords,
+      dependencies: pkg.dependencies,
+      peerDependencies: pkg.peerDependencies,
+    },
+    null,
+    2,
+  );
+}
