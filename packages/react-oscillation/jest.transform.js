18091c0c460f27bcc05b61f889492ab5e8c857ca
Oleksii Raspopov <oleksii.raspopov@gmail.com>
2024-09-24 23:41:25 -0400

    2024

--- packages/react-oscillation/jest.transform.js
+++
@@ -1,5 +0,0 @@
-import bj from "babel-jest";
-
-export default bj.createTransformer({
-  presets: [["@babel/preset-env", { loose: true }], "@babel/preset-react"],
-});
--- packages/react-oscillation/modules/spring.js
+++
@@ -1,5 +0,0 @@
-import { spring, springs } from "oscillation";
-
-// Re-exporting spring configs from the core library so the users
-// won't need to bother remembering different endpoints to import from
-export { spring, springs };
