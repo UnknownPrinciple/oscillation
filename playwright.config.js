9356fc26492bb69d42b506b6c6f2d04b20c2eaf6
Oleksii Raspopov <oleksii.raspopov@gmail.com>
2024-10-17 16:51:32 -0400

    use headless mode on ci

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "@playwright/test";
 
+let CI = process.env.CI != null;
+
 export default defineConfig({
-  use: { headless: false },
+  use: { headless: CI },
   testMatch: /.*\.spec\.js/,
   reporter: [["list"], ["rollwright/coverage-reporter"]],
 });
